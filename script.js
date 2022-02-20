cdcebfd584532769efd8c1a737098248993bbb93
MaksimDovhyi <maximgovgiy@gmail.com>
2022-02-20 19:24:48 +0200

    Change loop

--- script.js
+++ script.js
@@ -9,17 +9,22 @@ let personelMovieDB = {
 };
 
 
-let a = prompt('Один из последних ваших фильмов');
+/*let a = prompt('Один из последних ваших фильмов');
 let b = prompt('На сколько его оцените');
 let c = prompt('Один из последних ваших фильмов');
-let d = prompt('На сколько его оцените');
+let d = prompt('На сколько его оцените');*/
 
-/*for(i = 0; i < 2; i++){
-    let first = prompt('Один из последних ваших фильмов');
-    let second = prompt('На сколько его оцените');
-};*/
+for(i = 0; i < 2; i++){
+    let a = prompt('Один из последних ваших фильмов');
+    let b = prompt('На сколько его оцените');
+    if(a === " " || a === null || a.length > 50 || b === " " || b === null || b.length > 50){
+    i--
+    }else{
+    personelMovieDB.movies[a] = b;
+    }
+};
 
-personelMovieDB.movies[a] = b;
-personelMovieDB.movies[c] = d;
+
+//personelMovieDB.movies[c] = d//;
 console.log(personelMovieDB);
 
